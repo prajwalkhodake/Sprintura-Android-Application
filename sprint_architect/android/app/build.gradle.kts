cea8418c64dbaa0fe58f835e3c33bacf8d9efd94
Prajwal Khodake <prajwalkhodake@Prajwals-MacBook-Air.local>
2026-05-20 13:14:53 +0530

    Update ndkVersion to 27.0.12077973

--- sprint_architect/android/app/build.gradle.kts
+++ sprint_architect/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.sprintarchitect.sprint_architect"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
